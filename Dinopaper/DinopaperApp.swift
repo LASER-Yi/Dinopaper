67665e3115953e054976e9f7a3610629fff9a24a
LASER-Yi <liangyi0007@gmail.com>
2021-12-03 22:38:32 +0800

    Init

---
+++ Dinopaper/DinopaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinopaperApp.swift
+//  Dinopaper
+//
+//  Created by LiangYi on 2021/12/3.
+//
+
+import SwiftUI
+
+@main
+struct DinopaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
